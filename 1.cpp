0412ec132b31af3a3b564f0974c155c663d3d952
Janaka Pruthuvi Vimukthi Muthunayake <118558852+Pruthuvi001@users.noreply.github.com>
2023-03-31 15:38:39 +0530

    Tutorial 10

--- 1.cpp
+++ 1.cpp
@@ -1,22 +1,31 @@
 #include<stdio.h>
-#include<string.h>
-#define SIZE 10 //declare size as 10
 int main(void)
-{ //begins to execute the programme
-	int i; //counting variable
-	char word[SIZE]; //declare array
-	
-	printf("Enter the Word: "); //prpmpt
-	scanf("%s", &word); //string array
-	int s = strlen(word); //string length
-	
-	for (i = 0; i < (s / 2); i++){ //check if its a palindrome or not a palindrome
-		if(word[i] == word[(s - 1) - i]){ //comparing from the middle possition 
-			printf("%s is a Palindrome\n", word); //print palindrome word
-		}
-		else{ //or
-			printf("%s is not a Palindrome\n", word); //prints not palindrome word
-		}
-	}
+{ //begins to execute
+	int number, Inumber; //declare variables
+	FILE * numPtr; //decalre file pointer
+	numPtr = fopen("number.dat", "a+"); //open file to read and write
+	if(numPtr == NULL){ //check if the file is null
+		printf("Can't read a file"); //display an error
+		return -1; //exits the programme if null
+	} //end if statement
+	printf("Enter a Number: "); //prompt
+	scanf("%d", &Inumber); //read a number
+	fscanf(numPtr, "%d", &number); //read the values from file
+	while(!feof(numPtr)){ //read values until end of the file
+		if(Inumber == number){ //check the values in file and user inputs are same
+			printf("Error ! The number is already exists"); //display error
+			return -1; //exits if values are same
+		} //end if function
+		fscanf(numPtr, "%d", &number); //read the values from file
+	} //end while statement
+	fprintf(numPtr, "%d\t", Inumber); //store value in the file
+	fclose(numPtr); //close the file
+	numPtr= fopen("number.dat", "r"); //open the file to read
+	fscanf(numPtr, "%d", &number); //read the values from the file
+    while(!feof(numPtr)){ //read values until end of the file
+    	printf("%d\t", number); //display the values
+    	fscanf(numPtr, "%d", &number); //read the values from the file
+	} //end while statement
+    fclose(numPtr); //close the file
 	return 0; //return function
 } //end function main
--- 2.cpp
+++ 2.cpp
@@ -1,26 +1,31 @@
 #include<stdio.h>
-#define ROW 4 //define rows
-#define COLOUMN 4 //define coloumns
 int main(void)
-{ //begins to execute the programme
-	int no1[ROW][COLOUMN] = {{5,7,8,10}, {9,3,0,6}, {8,1,9,2}, {4,7,2,1}}; //decalre 1st mattrix
-	int no2[ROW][COLOUMN] = {{2,2,1,3}, {8,1,3,1}, {1,2,2,4}, {3,1,1,2}}; //declare 2nd mattix
-	int i, j, sum; //counting variables and sum variable
-	
-	for(i = 0; i <= 3; ++i){ //count until 3 values
-		for(j = 0; j <=3; ++j){ //count until 3 values
-			printf("%3d", no1[i][j]); //display values
-		}
-		printf("\n"); //net line
-	}
-	printf("\n"); //next line
-	
-	for(i = 0; i <= 3; ++i){ //count until 3 values
-		for(j = 0; j <= 3; ++j){ //count until 3 values
-			sum = no1[i][j] + no2[i][j]; //calculation sum of mattrix
-			printf("%3d", sum); //display the sum
-		}
-		printf("\n"); //next line
-	}
+{ //begins to execute
+	char name[10], type; //declare variables
+	int consulting = 0, scanning = 0, testing = 0; //declare variables
+	FILE * cfPtr; //decalre file pointer
+	cfPtr = fopen("appoinments.dat", "r"); //open file to read
+	if(cfPtr == NULL){ //check if the file is null
+		printf("Can't read a file"); //display an error
+		return -1; //exits the programme if null
+	} //end if statement
+	fscanf(cfPtr, "%s %c", name, &type); //read the values from file
+	while(!feof(cfPtr)){ //read values until end of the file
+		if(type == 'C'){ //check the appointment type
+			consulting++; //increase the type from 1
+		} //end if statement
+		else if(type == 'S'){ //check the appointment type
+				scanning++; //increase the type from 1
+			} //end if statement
+			else if(type == 'T'){ //check the appointment type
+					testing++; //increase the type from 1
+				} //end if statement
+		fscanf(cfPtr, "%s %c", name, &type); //read the values from file
+	} //end while statement
+	printf("Appointment Type\tNumber of Patients\n"); //display the headings
+	printf("Consulting\t\t%d\n", consulting); //display the consulting patients
+	printf("Scanning\t\t%d\n", scanning); //display the scanning patients
+	printf("Testing\t\t\t%d\n", testing); //display the testing patients
+	fclose(cfPtr); //close the file
 	return 0; //return function
 } //end function main
